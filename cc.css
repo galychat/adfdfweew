a522dfa5cd9f6d2b4deda879215b9956fbef21cf
galychat <42301824+galychat@users.noreply.github.com>
2018-08-30 14:08:36 +0300

    Create cc.css

---
+++ cc.css
@@ -0,0 +1,29 @@
+$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif;    height: 20px;"  onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">   مرحبا بكم في شات غالي </marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
+$('.uzr.fl.corner.borderg').css({
+	'margin': '0px 0',
+	borderRight: '2px solid FF2B41',
+	borderLeft: '2px solid FF2B41',
+	borderTop: '.40px solid FF2B41',
+	borderBottom: '.50px solid #c1202045 ',
+	paddingTop: '2px',
+	borderRadius: '10px'
+});
+$('.room.borderg.hand.nosel.fl').css({
+	'margin': '1px 0',
+	borderRight: '2px solid FF458F',
+	borderLeft: '2px solid FF458F',
+	borderTop: '1px solid FF458F',
+	borderBottom: '1px solid FF458F',
+	paddingTop: '2px',
+	borderRadius: '10px'
+});
+$(".corner.fa.fa-user.label.label-primary.fr.uc").css("border-radius", "15px")
+$(".fr.co").css("border-radius", "15px")
+$('.break.light').css({
+	"background-image": "url(https://galychat.com/sico/z1cla7quus10.png)"
+});
+$(` <center><div style=" width: 100%;height: auto!important; "  id="sa" margin: 2px;" class="fa fa-heartbeat btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp;مرحبا بكم في شات غالي &nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>â—ڈ ط¯ط±ط¯ط´ط©â—ڈ ط¨ظ†ط§طھâ—ڈ ط§ظ‡ظ„ظ†ط§  â—ڈ</p><audio controls loop="" play=""><source src="soon" "></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary')
+$(`<center><div><div width="99.5%" style="background-image:url(https://f.top4top.net/p_9714pjq31.gif);color: #C90000;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#ffffff;padding-bottom: 2px!important;" ><font style="background-color: #fff;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" > ط§ظ‡ظ„ط§ ظˆط³ظ‡ظ„ط§ ط¨ظƒظ… ظپظٹ ط´ط§طھ ط¨ظ†ط§طھ ط§ظ‡ظ„ظ†ط§ ظ„ظ„ط¬ظˆط§ظ„  </font></div></div><center>`).insertBefore('#d2');
+$(`<img style="height:50px;width: 100%;margin-right: 3px;background-color: #FFFFFF;" src="" border="0">`).insertBefore('div#d2bc');
